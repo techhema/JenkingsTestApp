83fc8eee1c47c8c1c176c0265c6bdb8608f9e8a8
HemaTechnamo <hema_koduri@yahoo.co.in>
2015-10-30 02:12:53 -0400

    Test

---
+++ JenkingsTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkingsTestApp.Startup))]
+namespace JenkingsTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
